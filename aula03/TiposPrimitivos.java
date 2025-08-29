6436559e824e046785afe7637e7637a201d8c8bf
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-08-29 08:29:53 -0300

    l

---
+++ aula03/TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package aula03;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        String nome = "Deyvid";
+        Float nota = 8.5f;
+        System.out.printf("A nota de %s é: %.2f%n", nome, nota);
+    }
+}
