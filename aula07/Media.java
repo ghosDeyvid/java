3c85f56e8eb66cfe3ec73324700b7dcd088a5837
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-10-03 10:02:53 -0300

    l

---
+++ aula07/Media.java
@@ -0,0 +1,19 @@
+package aula07;
+
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("primeira nota ");
+        float n1 = teclado.nextFloat();
+        System.out.println("Segunda nota");
+        float n2 = teclado.nextFloat();
+        float m = (n1 + n2)/2;
+        System.out.println("A media é " + m);
+        if(m>9){
+            System.out.println("Aprovado com louvor");
+        }
+    }
+    
+}
