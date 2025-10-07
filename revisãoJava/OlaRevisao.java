7de157f68c86a1507992ccff01a86c035c051a23
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-10-07 12:42:56 -0300

    l

---
+++ revisãoJava/OlaRevisao.java
@@ -0,0 +1,15 @@
+public class OlaRevisao {
+    public static void main(String[] args) {
+        byte NumeroPequeno = 127;
+        short NumeroMedio = 32767;
+        int NumeroGrande = 2147483647;
+        long NumeroGigante = 9223372036854775807L;
+
+
+        System.out.println(NumeroPequeno);
+        System.out.println(NumeroMedio);
+        System.out.println(NumeroGrande);
+        System.out.println(NumeroGigante);
+    }
+    
+}
