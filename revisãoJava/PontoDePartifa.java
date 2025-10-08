c2cad2891cdf4aa1f88be12597717c7e229c910d
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-10-08 11:04:49 -0300

    l

---
+++ revisãoJava/PontoDePartifa.java
@@ -0,0 +1,12 @@
+public class PontoDePartifa {
+    public static void main(String[] args) {
+        double salario = 1799.99;
+        float bonos = 250.12f;
+        
+        double salariofinal = salario + bonos;
+
+        System.out.printf(" Meu salario total é %.2f" , salariofinal);
+
+    }
+    
+}
