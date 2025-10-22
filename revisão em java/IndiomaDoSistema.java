61befb11d3bcb5d593642573324b981ccc559759
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-10-22 10:14:25 -0300

    l

---
+++ revisão em java/IndiomaDoSistema.java
@@ -0,0 +1,10 @@
+
+import java.util.Locale;
+
+public class IndiomaDoSistema {
+    public static void main(String[] args) {
+        Locale Idioma = Locale.getDefault();
+        System.out.println("O idioma do sistema e : " + Idioma.getDisplayLanguage());
+    }
+    
+}
