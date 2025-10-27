19bccbd867510f4f5f218742675d00d7c50d7b11
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-10-27 11:26:23 -0300

    l

---
+++ revisão em java/OperadoresAriti.java
@@ -0,0 +1,8 @@
+public class OperadoresAriti {
+    public static void main(String[] args) {
+        int n1 = 3;
+        int valor = 5 + ++n1;
+        System.out.println(valor);
+        System.out.println(n1);
+    }
+}
