19f20449e6505a1867e90f8977219297278efa4e
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-10-28 11:56:41 -0300

    l

--- revisão em java/OperadoresAriti.java
+++ revisão em java/OperadoresAriti.java
@@ -1,8 +1,19 @@
 public class OperadoresAriti {
     public static void main(String[] args) {
-        int n1 = 3;
-        int valor = 5 + ++n1;
-        System.out.println(valor);
-        System.out.println(n1);
+        /*int n1 = 5;
+        int valor = 5 + n1++;
+        System.out.println(valor);*/
+
+       /*  int x = 4;
+        x *= 2;
+        System.out.println(x); */
+        
+        /*float v = 8.4f;
+        int ar = (int) Math.round(v);
+        System.out.println(ar);*/ 
+
+        double ale = Math.random();
+        int n = (int) (15 + ale * (50-15));
+        System.out.println(n);
     }
 }
---
+++ revisão em java/TiposDeVariaves.java
@@ -0,0 +1,12 @@
+public class TiposDeVariaves {
+    public static void main(String[] args) {
+        byte numeroPequeno = 127;
+        short numeroCurto = 32767;
+        int numeroMaior = 2147483647;
+
+        System.out.println(numeroPequeno);
+        System.out.println(numeroCurto);
+        System.out.println(numeroMaior);
+    }
+    
+}
