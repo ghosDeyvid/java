e6add517e335a4e3dfdd78a766deb1eb1529d23a
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-10-30 08:50:01 -0300

    l

--- revisão em java/TiposDeVariaves.java
+++ revisão em java/TiposDeVariaves.java
@@ -1,12 +1,29 @@
 public class TiposDeVariaves {
     public static void main(String[] args) {
+        /* Tipos de Variáveis inteiros */
         byte numeroPequeno = 127;
         short numeroCurto = 32767;
         int numeroMaior = 2147483647;
+        long numeroGigante = 9223372036854775807L;
+        /* Tipos de Variáveis flutuantes */
+        float numeroFlutuante = 3.14f; 
+        double numeroDuplo = 3.141592653589793;
+        /* Variavel do tipo caractere */
+        char caractere = 'A';
+        /* Variavel do tipo booleano */
+        boolean verdadeiro = true;
+        /* Variavel do tipo String */
+        String texto = "Olá, Mundo!";
 
         System.out.println(numeroPequeno);
         System.out.println(numeroCurto);
         System.out.println(numeroMaior);
+        System.out.println(numeroGigante);
+        System.out.println(numeroFlutuante);
+        System.out.println(numeroDuplo);
+        System.out.println(caractere);
+        System.out.println(verdadeiro);
+        System.out.println(texto);
     }
     
 }
