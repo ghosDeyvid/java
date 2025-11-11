da6c6c0aca0b082f833ee2af89df6586016f0d77
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-11-11 07:06:57 -0300

    l

---
+++ revisãoEmJava/OperadorTernario.java
@@ -0,0 +1,12 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        /*double nota = 5.6;
+        String resultado = nota >=7 ? "aprovado": "reprovado";
+        System.out.println(resultado);*/
+
+        int numero = 7;
+        String ParouImpar = numero %2 ==0 ? "Par": "Impar";
+        System.out.println(ParouImpar);
+    }
+    
+}
