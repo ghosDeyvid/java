599c1b3c36946f6ab7a79615926f3d069294442d
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-12-22 12:03:42 -0300

    l

--- revisãoEmJava/Contador02.java
+++ revisãoEmJava/Contador02.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Contador02 {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
+        /*Scanner teclado = new Scanner(System.in);
         System.out.print("Digite o valor final do contador: ");
         int valorFinal = teclado.nextInt();
         
@@ -11,6 +11,15 @@ public class Contador02 {
             c++;
             System.out.println("Cabanhota" + c);
         }
-        teclado.close();
-}
+        teclado.close();*/
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o valor inicial do contador: ");
+        int valor = teclado.nextInt();
+        int c= 0;
+        while ( c < valor){
+            c++;
+            System.out.println(" Deus e fiel " + c);
+        }
+ }
 }
