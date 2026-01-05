9dccaa32b3b19e484e4bf9406e3057c169fb6e8d
deyvid siqueira miguel <deyvidmycny@gmail.com>
2026-01-05 18:32:40 -0300

    l

---
+++ revisãoEmJava/fucao02.java
@@ -0,0 +1,12 @@
+public class fucao02 {
+     static int soma (int a, int b){
+        int s=a+b;
+        return s;
+     }
+    public static void main(String[] args) {
+        System.out.println("Começou a programa");
+        int sm=soma(5,2);
+        System.out.println("Soma vale: " + sm);
+    }
+    
+}
